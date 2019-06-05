f152e1e3faedcc04a63aef741f87fbfdebd45674
Ashik Banger Veerappa <44420503+ashikbv@users.noreply.github.com>
2019-06-05 12:19:35 -0400

    Update credentials.py
    added values to variables

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "AC1e49dd080620af990bb54713533d3e24"
+auth_token = "68615e281aac525ed31ebb1c563444bf"
+my_cell = "+17184313956"
+my_twilio = "+19179702273"
